0f876bd2513d5c665adae83c335baad60aade801
ellie112 <100469674+ellie112@users.noreply.github.com>
2022-03-23 22:27:08 +0200

    Add files via upload

---
+++ LibrarySystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using LibrarySystem.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibrarySystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        private const string ConnectionSting = @"Server=.\SQLEXPRESS;Database=LibrarySystem;Trusted_Connection=True;Integrated Security=True;";
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=LibrarySystem;Trusted_Connection=True;Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<PersonBook>().HasKey(t => new { t.PersonId, t.BookId });
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Person> People { get; set; }
+        public DbSet<PersonBook> PersonBooks { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
